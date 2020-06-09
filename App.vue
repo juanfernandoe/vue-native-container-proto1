4172c23acffc3b38b0d3d57054134893b6d9d02c
Juan Fernando Estrada <fernandoe@supercodde.com>
2020-06-09 22:29:19 +1000

    Initial commit.

---
+++ App.vue
@@ -0,0 +1,33 @@
+<template>
+  <view class="container">
+    <web-view :source="{uri:'https://www.google.com/'}" 
+      :style="{marginTop: 20}" 
+      :originWhitelist="['*']"
+      javaScriptEnabled
+      domStorageEnabled
+      allowFileAccessFromFileURLs
+      startInLoadingState
+      mixedContentMode="compatibility"
+      />
+  </view>
+</template>
+
+<style>
+.container {
+  background-color: #fff;
+  flex: 1;
+}
+.text-color-primary {
+  color: blue;
+}
+</style>
+
+<script>
+import { WebView } from "react-native-webview";
+
+export default {
+  components: {
+    "web-view": WebView
+  }
+}
+</script>
